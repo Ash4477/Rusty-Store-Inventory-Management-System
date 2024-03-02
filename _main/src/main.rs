d238bd3583a0fcbe2bb758e440cdc4deafa63c4f
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-03-02 08:24:28 +0500

    add generate_report function to print a report of all the products in inventory

--- _main/src/main.rs
+++ _main/src/main.rs
@@ -1,82 +1,128 @@
 use std::io;
 
-struct Product {
-    name : String,
-    description: String,
-    price: f64,
-    quantity: i32,
-}
+// struct Product {
+//     name : String,
+//     description: String,
+//     price: f64,
+//     quantity: i32,
+// }
 
-impl Product {
-    fn edit_name(&mut self, new_name: String) {
-        self.name = new_name;
-    }
+// impl Product {
+//     fn edit_name(&mut self, new_name: String) {
+//         self.name = new_name;
+//     }
 
-    fn edit_price(&mut self, new_price: f64) {
-        self.price = new_price;
-    }
+//     fn edit_price(&mut self, new_price: f64) {
+//         self.price = new_price;
+//     }
 
-    fn edit_description(&mut self, new_desc: String) {
-        self.description = new_desc;
-    }
+//     fn edit_description(&mut self, new_desc: String) {
+//         self.description = new_desc;
+//     }
 
-    fn edit_quantity(&mut self, new_quantity: i32) {
-        self.quantity = new_quantity;
-    }
-}
+//     fn edit_quantity(&mut self, new_quantity: i32) {
+//         self.quantity = new_quantity;
+//     }
+// }
 
 fn main() {
 
-    if !admin_check() {
-        println!("Sorry, wrong credentials :<\nExiting System!");
-        return;
+    // if !admin_check() {
+    //     println!("Sorry, wrong credentials :<\nExiting System!");
+    //     return;
+    // }
+
+    // let mut products : Vec<i32> = Vec::new();
+
+    let mut menu_choice: i32 = 0;
+
+    println!("\tRusty Inventory Management\n----------------------------------------
+    => 1: Add Product
+    => 2: Edit Product Info
+    => 3: Delete Product
+    => 4: Exit System
+    \tEnter your choice:
+    ");
+
+    while menu_choice != 0 {
+        menu_choice = integer_input();  
+        match menu_choice {
+            1 => addProduct(),
+            2 => editProduct(),
+            3 => deleteProduct(),
+            4 => return,
+            _ => println!("Choose a valid option!\nEnter:"),
+        }
     }
-
-    let mut products : Vec<i32> = Vec::new();
-
-    // println!("\tRusty Inventory Management\n----------------------------------------
-    // => Add Product
-    // => Edit Product Info
-    // => Delete Product
-    // ");
 
 }
 
-fn admin_check() -> bool {
+fn integer_input() {
+    let mut input = String::new();
+    let mut parsed_input: Result<i32, _>;
+    loop {
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        parsed_input= input.trim().parse();
 
-    // For the sake of simplicity, I have set admin as login user and password!
-    let user_check_val : String = String::from("admin");
-    let pass_check_val : String = String::from("admin");
+        match parsed_input {
+            Ok(num) => {
+                break;
+            },
+            Err(_) => println!("Invalid input!\nPlease enter an integer:"),
+        } 
+    }
 
-    println!("\n----------------------------------------\n\tRusty Inventory Management\n----------------------------------------");
-    println!("\t      'Login Panel'\n");
+    parsed_input
+}
 
-    let mut user_name = String::new();
-    println!("Enter your username: ");
-    io::stdin().read_line(&mut user_name)
-    .expect("Failed to read line");
+// fn admin_check() -> bool {
 
-    let mut user_pass = String::new();
-    println!("Enter your password: ");
-    io::stdin().read_line(&mut user_pass)
-    .expect("Failed to read line");
+//     // For the sake of simplicity, I have set admin as login user and password!
+//     let user_check_val : String = String::from("admin");
+//     let pass_check_val : String = String::from("admin");
 
-    if user_name.trim() == user_check_val && user_pass.trim() == pass_check_val {
-        true
+//     println!("\n----------------------------------------\n\tRusty Inventory Management\n----------------------------------------");
+//     println!("\t      'Login Panel'\n");
+
+//     let mut user_name = String::new();
+//     println!("Enter your username: ");
+//     io::stdin().read_line(&mut user_name)
+//     .expect("Failed to read line");
+
+//     let mut user_pass = String::new();
+//     println!("Enter your password: ");
+//     io::stdin().read_line(&mut user_pass)
+//     .expect("Failed to read line");
+
+//     if user_name.trim() == user_check_val && user_pass.trim() == pass_check_val {
+//         true
+//     }
+
+//     else {
+//         false
+//     }
+// }
+
+
+
+fn addProduct() {}
+fn editProduct() {}
+fn deleteProduct() {}
+
+fn generate_report(products: &Vec<i32>) {
+    if products.len() <=0 {
+        println!("Products List is Empty :<");
     }
 
     else {
-        false
+        println!("\t      'Inventory Report'\n");
+        println!("-------------------");
+        for (name, description, price, quantity) in products {
+            println!("Name: {}", name);
+            println!("Description: {}", description);
+            println!("Price: ${:.2}", price);
+            println!("Quantity: {}", quantity);
+            println!("-------------------");
+        }
     }
 }
-
-// fn generate_report(products: &Vec<i32>) {
-//     if products.len() <=0 {
-//         println!("Products List is Empty :<");
-//         return;
-//     }
-
-//     for item in products.iter() {
-//         println!("{}",item);
-//     }
-// }
